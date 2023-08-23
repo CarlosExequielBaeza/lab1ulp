d91c71b9432f34989fd73ef3d84a74dd368af922
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-08-23 19:50:51 -0300

    gabyyy

--- TP2/TP2LAB.java
+++ TP2/TP2LAB.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Profe soy el colaborador no tenemos compatibles los netbeans por eso comento aca , yo este tp igual lo hice...
+//El Administrador si esta haciendo todo atravez de github desktop y Netbeans...
 package tp2lab;
 
 /**
