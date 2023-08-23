d25f558fb8724c87344024b9944b21d13af78eb6
Carlos Baeza <132070988+CarlosExequielBaeza@users.noreply.github.com>
2023-08-23 20:13:24 -0300

    Carga32

---
+++ segundapruebanetbeans17/src/main/java/com/mycompany/segundapruebanetbeans17/Segundapruebanetbeans17.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+// asdasñlkdkñlaslkdklñaslñd
+package com.mycompany.segundapruebanetbeans17;
+
+/**
+ *
+ * @author litob
+ */
+public class Segundapruebanetbeans17 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
