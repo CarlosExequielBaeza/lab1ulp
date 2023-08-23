6efaf263cde2b707c511a7b5f14957eddf56d7a7
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-08-23 20:21:03 -0300

    godetoooooo

--- segundapruebanetbeans17/src/main/java/com/mycompany/segundapruebanetbeans17/Segundapruebanetbeans17.java
+++ segundapruebanetbeans17/src/main/java/com/mycompany/segundapruebanetbeans17/Segundapruebanetbeans17.java
@@ -12,5 +12,9 @@ public class Segundapruebanetbeans17 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        for(int i=1; i>0;i++){
+            System.out.println("Godetoooooooooooooooo");
+        }
     }
 }
