6b244bf4552345507cedc1313849f9ad631167bf
Carlos Baeza <132070988+CarlosExequielBaeza@users.noreply.github.com>
2023-08-23 20:00:13 -0300

    Carga

--- TP2/TP2LAB.java
+++ TP2/TP2LAB.java
@@ -1,6 +1,7 @@
 //Profe soy el colaborador no tenemos compatibles los netbeans por eso comento aca , yo este tp igual lo hice...
 //El Administrador si esta haciendo todo atravez de github desktop y Netbeans...
 //Modificacion por Carlos Baeza
+//otra modificacion desde netbeans
 package tp2lab;
 
 /**
