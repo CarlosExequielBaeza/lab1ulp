0bed8c288bc9fe9e2b1ab6cbffc5d92eea8ba839
Carlos Baeza <132070988+CarlosExequielBaeza@users.noreply.github.com>
2024-03-16 20:00:16 -0300

    Cargas1

---
+++ Pruebanetbeans2024/src/main/java/com/mycompany/pruebanetbeans2024/Pruebanetbeans2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebanetbeans2024;
+
+/**
+ *
+ * @author litob
+ */
+public class Pruebanetbeans2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
