9ecef487536bbcc6b3dd8716eb375ac6b4abff13
Carlos Baeza <132070988+CarlosExequielBaeza@users.noreply.github.com>
2024-03-20 18:13:59 -0300

    Carga1

---
+++ PRACTICO2/src/main/java/com/mycompany/practico2/PRACTICO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practico2;
+
+/**
+ *
+ * @author Carlos Baeza
+ */
+public class PRACTICO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Practico 2");
+    }
+}
